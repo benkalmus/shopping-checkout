7533197330c6d0a8b8f1397d2bd20d1f29904d52
Ben Kalmus <kalmusb@gmail.com>
2024-07-20 10:42:33 +0100

    add: table driven tests for items without discounts

--- shopping_test.go
+++ shopping_test.go
@@ -1,6 +1,7 @@
 package shopping
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -31,5 +32,46 @@ func TestCheckout(t *testing.T) {
 			t.Fatalf("Expected total price %d got %d", price, totalPrice)
 		}
 	})
+}
+
+func TestCheckoutWithoutDiscount(t *testing.T) {
+	//setup test
+	itemPriceMap := map[string]int{
+		"A": 50,
+		"B": 30,
+		"C": 20,
+		"D": 15,
+	}
+
+	cases := []struct {
+		items []string
+		total int
+	}{
+		{[]string{"A", "B"}, 80},
+		{[]string{"A", "A"}, 100},
+		{[]string{"A", "B", "C", "D"}, 115},
+		{[]string{"A", "A", "A", "C"}, 170},
+		{[]string{"A", "A", "C", "C"}, 140},
+	}
+
+	for _, testData := range cases {
+		t.Run(fmt.Sprintf("%v returns total price %d", testData.items, testData.total), func(t *testing.T) {
+			shopping := NewShoppingCheckout()
+			shopping.SetSKUToPriceMapping(itemPriceMap)
+			for _, item := range testData.items {
+				err := shopping.Scan(item)
+				if err != nil {
+					t.Fatalf("Unexpected error scanning item: %v", err)
+				}
+			}
+			gotTotal, err := shopping.GetTotalPrice()
+			if err != nil {
+				t.Fatalf("Unexpected error getting total price: %v", err)
+			}
+			if gotTotal != testData.total {
+				t.Fatalf("got %q, want %q", gotTotal, testData.total)
+			}
+		})
+	}
 
 }
